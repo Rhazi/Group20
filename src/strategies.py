68a3329dae968eaa80014a2d84c7b3af6789587f
Mohammed Rhazi <48953471+Rhazi@users.noreply.github.com>
2025-09-19 12:17:49 -0500

    Update strategies.py

--- src/strategies.py
+++ src/strategies.py
@@ -26,3 +26,38 @@ class MovingAverageCrossoverStrategy(Strategy):
                 signals.append(('SELL', tick.symbol, 100, tick.price))
         
         return signals
+
+
+class macd(Strategy): # moving average convergence divergence 
+    def __init__(self, short_window: int = 15, large_window: int = 30, macd_window: int = 9): 
+        self.short_window = short_window
+        self.large_window = large_window
+        self.macd_window = macd_window
+        self.prices = []
+    
+    def generate_signals(self, tick: MarketDataPoint) -> list:
+        self.prices.append(tick.price)
+        signals = []
+    
+        if len(self.prices) >= self.long_window:
+            fast_ema = sum(ema(self.prices[-self.short_window:], self.short_window))/self.short_window
+            slow_ema = sum(ema(self.prices[-self.large_window:], self.large_window))/self.long_window
+            macd_line = fast_ema - slow_ema
+            signal_line = sum(ema(macd_line, self.macd_window))/self.macd_window
+    
+            if macd_line > signal_line:
+                signals.append((OrderAction.BUY, tick.symbol, 100, tick.price))
+            else:
+                signals.append((OrderAction.SELL, tick.symbol, 100, tick.price))
+    
+        return signals
+
+def ema(prices, window): #exponential moving average
+    alpha = 2 / (window + 1)
+    v = []
+    prev = prices[0]
+    for p in prices:
+        ema = alpha * p + (1 - alpha) * prev
+        v.append(ema)
+        prev = ema
+    return v
