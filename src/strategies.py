9501f59bf1e6ef356699a521ed6277e673bc7b5a
Mohammed Rhazi <48953471+Rhazi@users.noreply.github.com>
2025-09-20 16:14:03 -0500

    Update strategies.py

--- src/strategies.py
+++ src/strategies.py
@@ -1,58 +1,62 @@
 from abc import ABC, abstractmethod
-from models import MarketDataPoint
+import pandas as pd
+import matplotlib.pyplot as plt
+from seb import ticker
+
 
 class Strategy(ABC):
     @abstractmethod
-    def generate_signals(self, tick:MarketDataPoint) -> list:
+    def generate_signals(self, tick) -> list:
         pass
 
+
 class MovingAverageCrossoverStrategy(Strategy):
-    def __init__(self, short_window:int=5, long_window:int=20):
+    def __init__(self, short_window: int = 5, long_window: int = 20):
         self.short_window = short_window
         self.long_window = long_window
         self.prices = []
-    
-    def generate_signals(self, tick:MarketDataPoint) -> list:
-        self.prices.append(tick.price)
+
+    def generate_signals(self, tick) -> list:
+        self.prices.append(tick)
         signals = []
-        
+
         if len(self.prices) >= self.long_window:
             short_ma = sum(self.prices[-self.short_window:]) / self.short_window
             long_ma = sum(self.prices[-self.long_window:]) / self.long_window
-            
+
             if short_ma > long_ma:
                 signals.append(('BUY', tick.symbol, 100, tick.price))
             elif short_ma < long_ma:
                 signals.append(('SELL', tick.symbol, 100, tick.price))
-        
+
         return signals
 
 
-class macd(Strategy): # moving average convergence divergence 
-    def __init__(self, short_window: int = 15, large_window: int = 30, macd_window: int = 9): 
+class macd(Strategy):  # moving average convergence divergence
+    def __init__(self, short_window: int = 15, large_window: int = 30, macd_window: int = 9):
         self.short_window = short_window
         self.large_window = large_window
         self.macd_window = macd_window
         self.prices = []
-    
-    def generate_signals(self, tick: MarketDataPoint) -> list:
-        self.prices.append(tick.price)
+
+    def generate_signals(self, tick) -> list:
+        self.prices.append(tick)
         signals = []
-    
-        if len(self.prices) >= self.long_window:
-            fast_ema = sum(ema(self.prices[-self.short_window:], self.short_window))/self.short_window
-            slow_ema = sum(ema(self.prices[-self.large_window:], self.large_window))/self.long_window
+
+        if len(self.prices) >= self.large_window:
+            fast_ema = self.ema(self.prices[-self.short_window:], self.short_window)[-1]
+            slow_ema = self.ema(self.prices[-self.large_window:], self.large_window)[-1]
             macd_line = fast_ema - slow_ema
-            signal_line = sum(ema(macd_line, self.macd_window))/self.macd_window
-    
+            signal_diff = [f-s for f,s in zip(self.ema(self.prices, self.short_window), self.ema(self.prices, self.large_window))]
+            signal_line = self.ema(signal_diff, self.macd_window)[-1]
             if macd_line > signal_line:
-                signals.append((OrderAction.BUY, tick.symbol, 100, tick.price))
+                signals.append(['BUY', tick.symbol, 100, tick.price])
             else:
-                signals.append((OrderAction.SELL, tick.symbol, 100, tick.price))
-    
+                signals.append(['SELL', tick.symbol, 100, tick.price])
+
         return signals
 
-    def ema(prices, window): #exponential moving average
+    def ema(self, prices, window) -> list:  # exponential moving average
         alpha = 2 / (window + 1)
         v = []
         prev = prices[0]
@@ -61,3 +65,30 @@ class macd(Strategy): # moving average convergence divergence
             v.append(ema)
             prev = ema
         return v
+
+#data = pd.read_csv('/Users/simorhazi/Desktop/uchicago/python/market_data.csv')
+#print(data)
+#strategie = macd()
+#r=[]
+#data["execution"] = None
+#print(data)
+#for i in range(data.shape[0]):
+#    signal = strategie.generate_signals(data.iloc[i]["price"])
+#    if not signal:signal = [0]
+#    data.loc[i,"execution"]=signal[0]
+#data["delta"] = data["price"].diff().fillna(0)
+#data["execution"] = data["execution"].shift(1).fillna(0)
+#data["pnl"] = data["delta"] * data["execution"]
+#data["cumpnl"] = data["pnl"].cumsum()
+
+#cash = 10
+#final_cash = cash+data["cumpnl"]
+
+#print(final_cash)
+#ticker = "AAPL"
+#plt.figure(figsize = (10,5))
+#plt.plot(data.index, data["cumpnl"])
+#plt.title(f"Cumulative profit and loss of {ticker} with a 1 share per trade trading policy")
+#plt.xlabel("Date")
+#plt.ylabel("Cumulative pnl")
+#plt.show()
