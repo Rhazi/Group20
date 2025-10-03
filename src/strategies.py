b184c96f3a8d415e9f96a5a40aa0f61c74154f01
Mohammed Rhazi <48953471+Rhazi@users.noreply.github.com>
2025-10-02 23:13:56 -0500

    Update strategies.py

--- src/strategies.py
+++ src/strategies.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from models import MarketDataPoint
 from models import OrderAction
-from collections import deque
+from collections import deque, defaultdict
 import statistics
 import pandas as pd
 
@@ -11,22 +11,22 @@ class Strategy(ABC):
         pass
 
 class Volatility(Strategy):
-    def __init__(self, k:float =1, prior_high:float = 0, prior_low:float = 0, atr:float = 2, equity:float = 10000, risk_pct:float = 0.01):
+    def __init__(self, k:float =0.1, atr: float = 1, equity: float = 10000, risk_pct: float = 0.01):
         self.__k=k
-        self.__prior_high=prior_high
-        self.__prior_low=prior_low
+        self.__prior_high={str:float}
+        self.__prior_low={str:float}
         self.__atr=atr
         self.__equity=equity
         self.__risk_pct=risk_pct
-        
+
     def generate_signals(self, tick) -> list:
         signals = []
-        
-        long_threshold = self.__prior_high + self.__k * self.__atr
-        short_threshold = self.__prior_low - self.__k * self.__atr
-        
-        self.__prior_high=tick.high
-        self.__prior_low=tick.low
+
+        long_threshold = self.__prior_high.get(tick.symbol,0) + self.__k * self.__atr
+        short_threshold = self.__prior_low.get(tick.symbol,0) - self.__k * self.__atr
+
+        self.__prior_high[tick.symbol] = tick.high
+        self.__prior_low[tick.symbol] = tick.low
 
         # max capital at risk per trade
         max_risk = self.__equity * self.__risk_pct
@@ -34,14 +34,14 @@ class Volatility(Strategy):
         quantity = int(max_risk // per_share_risk)
 
         # check breakout conditions
-        if tick.price >= long_threshold and quantity > 0:
-            signals.append((OrderAction.BUY.value, tick.symbol, quantity, tick.price))
-        
-        elif tick.price <= short_threshold and quantity > 0:
-            signals.append((OrderAction.SELL.value, tick.symbol, quantity, tick.price))
+        if tick.open >= long_threshold and quantity > 0:
+            signals.append((tick.timestamp, OrderAction.BUY.value, tick.symbol, 1, tick.open))
+
+        elif tick.open <= short_threshold and quantity > 0:
+            signals.append((tick.timestamp, OrderAction.SELL.value, tick.symbol, 1, tick.open))
 
         else:
-            signals.append((OrderAction.HOLD.value, tick.symbol, quantity, tick.price))
+            signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, 1, tick.open))
 
         return signals
 
@@ -95,7 +95,7 @@ class macd(Strategy):  # moving average convergence divergence
         self.__prices = []
 
     def generate_signals(self, tick) -> list:
-        self.__prices.append(tick.price)
+        self.__prices.append(tick.close)
         signals = []
 
         if len(self.__prices) >= self.__large_window:
@@ -107,15 +107,15 @@ class macd(Strategy):  # moving average convergence divergence
             if macd_line > signal_line:
                 if self.__prev == OrderAction.BUY.value:
                     self.__prev = OrderAction.HOLD.value
-                    signals.append((OrderAction.HOLD.value, tick.symbol, 100, tick.price))
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, 100, tick.close))
                 else:
-                    signals.append((OrderAction.BUY.value, tick.symbol, 100, tick.price))
+                    signals.append((tick.timestamp, OrderAction.BUY.value, tick.symbol, 100, tick.close))
             else:
                 if self.__prev == OrderAction.SELL.value:
                     self.__prev = OrderAction.HOLD.value
-                    signals.append((OrderAction.HOLD.value, tick.symbol, 100, tick.price))
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, 100, tick.close))
                 else:
-                    signals.append((OrderAction.SELL.value, tick.symbol, 100, tick.price))
+                    signals.append((tick.timestamp, OrderAction.SELL.value, tick.symbol, 100, tick.close))
 
         return signals
 
@@ -138,7 +138,7 @@ class BollingerBandsStrategy(Strategy):
         self.__prices = deque(maxlen=window)
 
     def generate_signals(self, tick: MarketDataPoint) -> list:
-        self.__prices.append(tick.price)
+        self.__prices.append(tick.close)
         signals = []
 
         if len(self.__prices) >= self.__window:
@@ -148,12 +148,125 @@ class BollingerBandsStrategy(Strategy):
             upper_band = ma + self.__num_std * std
             lower_band = ma - self.__num_std * std
 
-            if tick.price < lower_band:
-                signals.append((OrderAction.BUY.value, tick.symbol, self.__qty, tick.price))
-            elif tick.price > upper_band:
-                signals.append((OrderAction.SELL.value, tick.symbol, self.__qty, tick.price))
+            if tick.close < lower_band:
+                signals.append((tick.timestamp, OrderAction.BUY.value, tick.symbol, self.__qty, tick.close))
+            elif tick.close > upper_band:
+                signals.append((tick.timestamp, OrderAction.SELL.value, tick.symbol, self.__qty, tick.close))
             else:
-                signals.append((OrderAction.HOLD.value, tick.symbol, self.__qty, tick.price))
+                signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
 
         return signals
+
+
+class MACD(Strategy):
+    def __init__(self, short_window: int = 12, long_window: int = 26, signal_window: int = 9, qty: int = 1):
+        self.__short_window = short_window
+        self.__long_window = long_window
+        self.__signal_window = signal_window
+        self.__qty = qty
+        self.__prices = deque(maxlen=self.__long_window)
+        self.__prev_action = OrderAction.HOLD.value  # to check cross over
+
+        # to calculate signal, using macd
+        self.__macd_history = deque(maxlen=self.__signal_window)  # Signal line
+        self.__fast_ema_prev = None
+        self.__slow_ema_prev = None
+        self.__signal_ema_prev = None
+
+    def generate_signals(self, tick):
+        self.__prices.append(tick.close)
+        signals = []
+
+        if len(self.__prices) >= self.__long_window:
+            # MACD line, using EMA of price
+            # decide alpha : smoothing weight, typically set as '2'
+            alpha_fast = 2 / (self.__short_window + 1)
+            alpha_slow = 2 / (self.__long_window + 1)
+            price = tick.close
+
+            # calculate moving average and MACD line as definition
+            self.__fast_ema_prev = alpha_fast * price + (1 - alpha_fast) * (self.__fast_ema_prev or price)
+            self.__slow_ema_prev = alpha_slow * price + (1 - alpha_slow) * (self.__slow_ema_prev or price)
+            # save in macd_history to calculate signal line
+            macd_line = self.__fast_ema_prev - self.__slow_ema_prev
+            self.__macd_history.append(macd_line)
+
+            # EMA of Signal Line
+            # decide alpha : smoothing weight typically set as '2'
+            alpha_signal = 2 / (self.__signal_window + 1)
+            # calculate moving average and signal line as defintion
+            self.__signal_ema_prev = alpha_signal * macd_line + (1 - alpha_signal) * (self.__signal_ema_prev or macd_line)
+
+            # select signals
+            if macd_line > self.__signal_ema_prev:
+                if self.__prev_action != OrderAction.BUY.value:  # signal must came from neutral status
+                    signals.append((tick.timestamp, OrderAction.BUY.value, tick.symbol, self.__qty, tick.close))
+                    self.__prev_action = OrderAction.BUY.value
+                else:
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
+            else:
+                if self.__prev_action != OrderAction.SELL.value:  # signal must came from neutral status
+                    signals.append((tick.timestamp, OrderAction.SELL.value, tick.symbol, self.__qty, tick.close))
+                    self.__prev_action = OrderAction.SELL.value
+                else:
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
+
+        return signals
+
+class RSI(Strategy):
+    def __init__(self, period: int = 14, oversold: int = 30, overbought: int = 70, qty: int = 1):
+        self.__period = period
+        self.__oversold = oversold
+        self.__overbought = overbought
+        self.__qty = qty
+        self.__prices = defaultdict(list)
+        self.__prev_action = {}
+
+    def generate_signals(self, tick: MarketDataPoint) -> list:
+        self.__prices[tick.symbol].append(tick.close)
+        if tick.symbol not in self.__prev_action:
+            self.__prev_action[tick.symbol] = OrderAction.HOLD.value
+        signals = []
+
+        if len(self.__prices[tick.symbol]) > self.__period:
+            delta = [self.__prices[tick.symbol][i] - self.__prices[tick.symbol][i - 1] for i in range(1, len(self.__prices[tick.symbol]))]
+            ups = [d if d > 0 else 0 for d in delta]
+            downs = [-d if d < 0 else 0 for d in delta]
+
+            roll_up = self.ewm(ups, self.__period)
+            roll_down = self.ewm(downs, self.__period)
+            rs = [u / d if d != 0 else 0 for u, d in zip(roll_up, roll_down)]
+            rsi = [100 - (100 / (1 + r)) for r in rs]
+
+            current_rsi = rsi[-1]
+
+            if current_rsi < self.__oversold:
+                if self.__prev_action[tick.symbol] != OrderAction.BUY.value:
+                    signals.append((tick.timestamp, OrderAction.BUY.value, tick.symbol, self.__qty, tick.close))
+                    self.__prev_action[tick.symbol] = OrderAction.BUY.value
+                else:
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
+            elif current_rsi > self.__overbought:
+                if self.__prev_action[tick.symbol] != OrderAction.SELL.value:
+                    signals.append((tick.timestamp, OrderAction.SELL.value, tick.symbol, self.__qty, tick.close))
+                    self.__prev_action[tick.symbol] = OrderAction.SELL.value
+                else:
+                    signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
+            else:
+                signals.append((tick.timestamp, OrderAction.HOLD.value, tick.symbol, self.__qty, tick.close))
+
+        return signals
+
+    @staticmethod
+    def ewm(values, period):
+        alpha = 1 / period
+        out = []
+        prev = values[0]
+        for v in values:
+            val = alpha * v + (1 - alpha) * prev
+            out.append(val)
+            prev = val
+        return out
+        
+
         
